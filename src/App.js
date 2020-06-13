d9b8a0ebafaf4c4c6ea5a7e2e612f0ad466eea03
“Sajid <sajidmbdinch@gmail.com>
2020-06-13 13:14:05 +0500

    create basic react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return <div><strong> HELLOW WORLD APP.JS update </strong> {props.name} Age={props.Age -10}
+        
+  
+    </div>
+  
+}
+
+export default App;
